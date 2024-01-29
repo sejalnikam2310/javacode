eafe7e61d958b5795b9be26f32cf8501c95853bd
sejalnikam2310 <sejalnikam2001@gmail.com>
2024-01-29 15:35:10 +0530

    create

---
+++ table1.sql
@@ -0,0 +1,11 @@
+--use petshop2;
+create table dogs(breed varchar(225),color varchar(224),age int,weight int,price int);
+insert into dogs values("lab","black",2,10,10000);
+select * from dogs;
+
+
+
+
+
+
+
